3c26903f07fb9b7391c60c9e14c33b7feee07d3b
陈皮 <3364107383@qq.com>
2024-04-05 10:33:32 +0800

    update

--- NDFA.cpp
+++ NDFA.cpp
@@ -1,9 +1,10 @@
 /*实现从NFA到DFA的转化
 由用户输入自动机 M = (Q, T, δ，q0, F)
-用stl容器set存储
-对δ构建一个结构来存储
+数组存储Q、T&δ，单独存储q0和F
+T&δ作为结构体，内容包括字母表、和该字母表对应的转移函数
 set的用法：https://cloud.tencent.com/developer/article/2301483
-从q0开始，在δ中寻找从q0开始所能到达的状态，存入集合中，若包含终结状态，则标记为终结状态
+用数组存储新状态的集合
+从q0开始，在δ中寻找从q0开始所能到达的状态，存入集合中
 */
 #include <iostream>
 #include <algorithm>
@@ -14,3 +15,7 @@ set的用法：https://cloud.tencent.com/developer/article/2301483
 #include <set>
 #include <vector>
 using namespace std;
+
+int main(){
+
+}
