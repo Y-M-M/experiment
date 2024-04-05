dec6097e8945d868dd7053b8f69c792cf4151932
陈皮 <3364107383@qq.com>
2024-04-05 09:38:45 +0800

    update

--- NDFA.cpp
+++ NDFA.cpp
@@ -1,5 +1,7 @@
 /*实现从NFA到DFA的转化
-由用户输入自动机 M = (Q, T, δ，q0, F)*/
+由用户输入自动机 M = (Q, T, δ，q0, F)
+用stl容器set存储
+对δ构建一个结构来存储，包括前一个状态、后一个状态和移动字符*/
 #include <iostream>
 #include <algorithm>
 #include <cstring>
