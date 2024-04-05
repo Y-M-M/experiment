1220766edee8b91d09c28579ac89c8c9c0a69f41
陈皮 <3364107383@qq.com>
2024-04-05 09:29:22 +0800

    update

---
+++ NDFA.cpp
@@ -0,0 +1,11 @@
+/*实现从NFA到DFA的转化
+由用户输入自动机 M = (Q, T, δ，q0, F)*/
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <utility>
+#include <map>
+#include <set>
+#include <vector>
+using namespace std;
