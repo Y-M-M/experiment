fb994a65492e70e911cbdec43907c9daaad391c0
陈皮 <3364107383@qq.com>
2024-04-05 11:10:04 +0800

    update

--- NDFA.cpp
+++ NDFA.cpp
@@ -16,6 +16,37 @@ set的用法：https://cloud.tencent.com/developer/article/2301483
 #include <vector>
 using namespace std;
 
-int main(){
 
+typedef struct transfer{
+    char sign;
+    char next;
+}Transfer;
+typedef struct Q{
+    char state;
+    vector<Transfer> trans;
+}Qs;
+
+char read()//忽略空格和回车
+{
+    char c = getchar();
+    while(c == ' ' || c == '\n') c = getchar();
+    return c;
+}
+
+int main()
+{
+    printf("本程序完成从NFA到DFA的转化，接下来请您输入需要进行转化的NFA：\n");
+    printf("请输入NFA状态的个数：\n");
+    int states_num = 0;
+    scanf("%d", &states_num);
+    printf("请依次输入状态名称，以空格或回车分隔不同状态(请保证状态名称是单个字符)：\n");
+    Qs state[states_num];
+    for(int i = 0; i < states_num; i++)  state[i].state = read();
+    printf("请输入NFA字母表的个数：\n");
+    int sign_num = 0;
+    scanf("%d", &sign_num);
+    char signs[sign_num];
+    printf("请依次输入NFA包含的字符，以空格或回车分隔不同的状态：\n");
+    for(int i = 0; i < sign_num; i++) signs[i] = read(); 
+    
 }
